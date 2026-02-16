e008341250b3547bf0cea8f083018bc0ec04ee7b
Jawahar28 <jawaharn246@gmail.com>
2026-02-16 23:33:44 +0530

    Defined views and urls

--- first_project/views/say_hello.py
+++ first_project/views/say_hello.py
@@ -1,4 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
 def say_hello(request):
     return HttpResponse("Hello, World!")
+
+def say_hello_with_name(request: HttpRequest, name):
+    print(request.headers)
+    return HttpResponse(f"Hello {name}")
