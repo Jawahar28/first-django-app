4d8564f158f0767b0824500923a1dfa70aaa3100
Jawahar28 <jawaharn246@gmail.com>
2026-02-18 00:35:28 +0530

    Admin and Models

--- bingo/admin.py
+++ bingo/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from bingo.models import User
+from bingo.models import User, Product
 
 # Register your models here.
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = ['username','name','email']
+    list_display = ['username','email']
+    search_fields = ['name']
+
+
+admin.site.register(Product)
--- bingo/models.py
+++ bingo/models.py
@@ -6,3 +6,10 @@ class User(models.Model):
     username = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     email = models.EmailField(max_length=50)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.FloatField()
+    stock = models.PositiveIntegerField()
+    description = models.CharField(max_length=255)
